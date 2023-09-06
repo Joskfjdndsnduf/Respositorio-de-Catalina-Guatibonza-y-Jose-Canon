35c091159e036ca503d66d285961b868d641875c
leylaaaa1 <hcatalinagm@live.com>
2023-09-05 20:40:56 -0500

    prepa

---
+++ Preparcial1/newtonrapson.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  4 21:26:52 2023
+
+@author: katedremur
+"""
+
+
+def f(x):
+    
+    fun= 3*x**5 + 5*x**4 - x**3
+    return fun
+  
+    
+def deriv (x):
+    
+    return 15*x**4 +20*x**3 -3*x**2
+        
+    
