c7ea2f13cfa16e9e1bf754b52ed11919227ecce2
joskfjdndsnduf <j.canonm@uniandes.edu.co>
2023-09-28 19:19:37 -0500

    6

---
+++ preparcial2def/punto6.py
@@ -0,0 +1,16 @@
+import numpy as np
+import sympy as sym
+
+def func(a):
+    return (np.sqrt(0.01**2-a**2))/(0.5+a) 
+
+def trapecio(f,a,b):
+
+    return (0.5*(b-a)*(f(a)+f(b)))
+
+#print(trapecio(func,-0.01,0.01))
+
+def simpson(f,a,b):
+    return ((b-a)/6)*(f(a)+4*f((a+b)/2))+f(b)
+
+print(simpson(func,-0.01,0.01))
