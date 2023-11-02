a8678a9bd439ce547d4769ee9e9b277aedd082a8
joskfjdndsnduf <j.canonm@uniandes.edu.co>
2023-11-02 16:04:54 -0500

    taller 3

--- gausred.py
+++ gausred.py
@@ -42,9 +42,10 @@ def reduccion(a):
 Valores propios
 """
         
-m=np.array([[1,2,-1],[1,0,1],[4,-4,5]])
-
-q=np.array([1,2,1])
+#m=np.array([[1,2,-1],[1,0,1],[4,-4,5]])
+m=np.array([[2,-1],[-4,2]])
+q=np.array([4,3])
+#q=np.array([7,8,9])
 
 def mew(q,m):
     a=np.matmul(q.T,m)
@@ -56,7 +57,7 @@ def potinv(m,q):
         j=mew(q,m)
     return j
 
-#print(potinv(m,q))
+print(potinv(m,q))
 
 
 
@@ -103,4 +104,4 @@ def potinv(m,q):
         j=mew(q,m)
     return j
 
-print(potinv(m,q))
+#print(potinv(m,q))
